82cbcefb0091de266e78d46b46fe6efa98865351
Aayush <aayush.sharma317@gmail.com>
2018-08-18 04:10:42 +0530

    Added values folder

--- src/components/common/BorderlessButton.js
+++ src/components/common/BorderlessButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
-import { onBackgroundColor, accentColor } from '../../Values/colors';
+import { accentColor } from '../../Values/colors';
 
 const BorderlessButton = ({ children, onPress }) => {
     return (
