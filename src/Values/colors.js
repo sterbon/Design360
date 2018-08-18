4edaa79980df70cabab17ca428027fd8e206982c
Aayush <aayush.sharma317@gmail.com>
2018-08-18 06:32:40 +0530

    Updated ui

--- src/Values/colors.js
+++ src/Values/colors.js
@@ -1,7 +1,7 @@
 export const primaryColor = '#b5007b';
-export const accentColor = '#FF4200';
-export const backgroundColor = '#1D2226';
-export const backgroundColorLight = '#293136';
-export const backgroundColorDark = '#0E1112';
+export const accentColor = '#C9FF4D';
+export const backgroundColor = '#30303D';
+export const backgroundColorLight = '#383847';
+export const backgroundColorDark = '#24242E';
 export const onBackgroundColor = '#ffffff';
 export const onBackgroundColorFaded = '#869FB3';
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header, Input, Card, CardSection, Button, SquareButton } from './common';
+import { Header, Input, Card, CardSection, Button, ButtonSolid, SquareButton } from './common';
+import { backgroundColor, accentColor } from '../Values/colors';
 
 const googleImg = require('../assets/google.png');
 const facebookImg = require('../assets/facebook.png');
@@ -19,21 +20,41 @@ class LoginPage extends Component {
                     </CardSection>
                     <CardSection>
                         <Button>
-                            Let's Go
+                            Login
                         </Button>
+                        <ButtonSolid>
+                            Sign Up
+                        </ButtonSolid>
                     </CardSection>
                     <CardSection>
-                        <Button>
-                            Join Us
-                        </Button>
-                    </CardSection>
-                    <CardSection>
-                        <SquareButton image={googleImg} />
-                        <SquareButton image={facebookImg} />
+                        <View style={styles.buttonContainerStyle}>
+                            <SquareButton 
+                                image={googleImg}
+                                style={{ width: 35, height: 35 }}
+                            />
+                            <SquareButton 
+                                image={facebookImg} 
+                                style={{ width: 35, height: 35 }}
+                            />
+                        </View>
                     </CardSection>
                 </Card>
             </View>
         );
+    }
+}
+
+const styles = {
+    buttonContainerStyle: {
+        flex: 1,
+        paddingLeft: 140,
+        paddingRight: 140,
+        paddingTop: 50,
+        paddingBottom: 30,
+        backgroundColor,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
     }
 }
 
